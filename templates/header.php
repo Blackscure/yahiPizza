9763f14d74049b515ae9bc5badae2fd4123f4234
blackscure <Wekesabuyahi@gmail.com>
2020-10-07 15:38:49 +0300

    templates folder

---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<head>
+    <title>Yahi Pizza</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Yahi Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+        </div>
+    </nav>
